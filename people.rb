59a3688698f1f915bbd71bbe4a46b089342bad19
Vicky L <vicky.sy.loo@gmail.com>
2016-03-24 14:16:19 -0400

    Student and Instructor Classes created

---
+++ people.rb
@@ -0,0 +1,13 @@
+# 1 - Create Student and Intructor Classes
+
+class Student
+  def learn
+    puts "I get it!"
+  end
+end
+
+class Instructor
+  def teach
+    puts "Everything in Ruby is an Object"
+  end
+end
