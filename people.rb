dbc3aa85e02703e76ed443bf09b31dfe248ca756
Vicky L <vicky.sy.loo@gmail.com>
2016-03-24 14:50:07 -0400

    exercise 1 completed, fixed 'end' bug in first method

--- people.rb
+++ people.rb
@@ -1,13 +1,42 @@
 # 1 - Create Student and Intructor Classes
 
-class Student
+class Person
+
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+
+  def greeting
+    puts "Hi, my name is #{name}"
+  end
+
+end
+
+class Student < Person
   def learn
     puts "I get it!"
   end
 end
 
-class Instructor
+class Instructor < Person
   def teach
     puts "Everything in Ruby is an Object"
   end
 end
+
+Chris = Instructor.new("Chris")
+
+puts Chris.greeting
+
+Christina = Student.new("Christina")
+
+puts Christina.greeting
+
+puts Chris.teach
+
+puts Christina.learn
+
+puts Christina.teach
+# This command will not work because the Student class cannot access the method
+# in the Instructor class.
